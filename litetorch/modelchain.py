29adb3244bc8014ea12e4ea5c743f0a82470a1be
jfpettit <jfp15b@my.fsu.edu>
2020-02-27 11:41:47 -0800

    fix modelchain docstrig

--- litetorch/modelchain.py
+++ litetorch/modelchain.py
@@ -6,26 +6,26 @@ import numpy as np
 from typing import Callable
 
 class ModelChain(nn.Module):
+    """
+    Daisy-chain a list of models together. 
+
+    :param model_list: list of models to chain
+    :param output_squeeze: whether or not to squeeze output
+
+    This class takes in a list of PyTorch modules and runs a forward pass through each of them, returning the output of the final module.
+    It is the user's responsibility to make sure all input/output dimensions between modules match.
+    The modules should be initialized before being fed to ModelChain.
+
+    Example:
+        mlp1 = MLP(input_size=10, output_size=2)
+        mlp2 = MLP(input_size=2, output_size=10)
+        chained = ModelChain([mlp1, mlp2])
+
+        x = torch.randn(5, 10)
+        out = chained(x)
+
+    """
     def __init__(self, model_list, output_squeeze=False):
-        """
-        Daisy-chain a list of models together. 
-
-        :param model_list: list of models to chain
-        :param output_squeeze: whether or not to squeeze output
-
-        This class takes in a list of PyTorch modules and runs a forward pass through each of them, returning the output of the final module.
-        It is the user's responsibility to make sure all input/output dimensions between modules match.
-        The modules should be initialized before being fed to ModelChain.
-
-        Example:
-            mlp1 = MLP(input_size=10, output_size=2)
-            mlp2 = MLP(input_size=2, output_size=10)
-            chained = ModelChain([mlp1, mlp2])
-
-            x = torch.randn(5, 10)
-            out = chained(x)
-
-        """
         super(ModelChain, self).__init__()
         self.model_list = nn.ModuleList(model_list)
         self.output_squeeze = output_squeeze
